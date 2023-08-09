2e54d70e6998ce8915a92e4a9f89caec4c74838e
Gilbert R <gilbert@megsoftconsulting.com>
2023-08-09 14:02:29 -0400

    add logic to filter and paginate table

--- src/Components/productsTable/ProductsTable.tsx
+++ src/Components/productsTable/ProductsTable.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState, useMemo } from 'react';
 import { Product } from '../../features/products/models';
 import './ProductsTable.css';
 
@@ -6,39 +7,111 @@ interface Props {
 }
 
 const ProductsTable: React.FC<Props> = ({ data }) => {
+  const pageSize = 5;
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    paginateProducts(currentPage);
+  }, [data]);
+
+  const paginateProducts = (page: number) => {
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const result = data.slice(startIndex, endIndex);
+    setFilteredProducts(result);
+  };
+
+  const totalPages: number = useMemo(
+    () => Math.ceil(data.length / pageSize),
+    [data],
+  );
+
+  const handlePagination = (page: any) => {
+    paginateProducts(page);
+    setCurrentPage(page);
+  };
+
+  const isWhitespaceString = (input: string) => {
+    return /^\s*$/.test(input);
+  };
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+    if (isWhitespaceString(term)) {
+      paginateProducts(currentPage);
+    } else {
+      const filteredItems = data.filter((p) =>
+        p.name.toLocaleLowerCase().includes(term.toLocaleLowerCase()),
+      );
+      setFilteredProducts(filteredItems);
+    }
+    setCurrentPage(1);
+  };
   return (
-    <table className="products-table">
-      <thead>
-        <tr>
-          <th>Logo</th>
-          <th>Nombre del producto</th>
-          <th>Descripcion</th>
-          <th>Fecha de liberacion</th>
-          <th>Fecha de revision</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((p) => (
-          <tr key={p.id}>
-            <td>
-              <img
-                src={p.logo}
-                style={{
-                  width: '30%',
-                }}
-              />
-            </td>
-            <td>{p.name}</td>
-            <td>{p.description}</td>
-            <td>{p.date_release}</td>
-            <td>{p.date_revision}</td>
-            <td>...</td>
-          </tr>
-        ))}
-      </tbody>
-      <tfoot>{data.length} Resultados</tfoot>
-    </table>
+    <>
+      <div className="products-table-action-header">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="input"
+          value={searchTerm}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+        <div>Boton Agregar</div>
+      </div>
+      <div className="products-table-container">
+        <table className="products-table">
+          <thead>
+            <tr>
+              <th>Logo</th>
+              <th>Nombre del producto</th>
+              <th>Descripcion</th>
+              <th>Fecha de liberacion</th>
+              <th>Fecha de revision</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredProducts.map((p) => (
+              <tr key={p.id}>
+                <td>
+                  <img
+                    src={p.logo}
+                    style={{
+                      width: '30%',
+                    }}
+                  />
+                </td>
+                <td>{p.name}</td>
+                <td>{p.description}</td>
+                <td>{p.date_release}</td>
+                <td>{p.date_revision}</td>
+                <td>...</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={5}>{data.length} Resultados</td>
+              <td>
+                <select
+                  className="input"
+                  value={currentPage}
+                  onChange={(e) => handlePagination(Number(e.target.value))}>
+                  {Array.from({ length: totalPages }, (_, index) => (
+                    <option key={index + 1} value={index + 1}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </>
   );
 };
 
