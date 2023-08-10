06d51e08a9781dcc8d0b21d3465bebc31270d81f
Gilbert R <gilbert@megsoftconsulting.com>
2023-08-10 09:44:43 -0400

    begin working with form

--- src/pages/product-details/ProductDetails.tsx
+++ src/pages/product-details/ProductDetails.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Button from '../../Components/button/Button';
+import Page from '../../Components/page/Page';
+import styles from './ProductDetails.module.css';
 
 type ProductDetailsProps = {};
 
 const ProductDetails: React.FC<ProductDetailsProps> = () => {
   const { id } = useParams();
-  return <div>Products Details {id}</div>;
+  return (
+    <Page style={{ maxWidth: '700px' }}>
+      <>
+        <h1 className={styles.heading}>Formulario de Registro</h1>
+        <form>
+          <div className={styles.row}>
+            <div className={styles.col}>
+              <div className="form-group">
+                <label className="form-group__label">ID</label>
+                <input type="text" className="form-group__input" />
+              </div>
+
+              <div className="form-group">
+                <label className="form-group__label">Descripción</label>
+                <input type="text" className="form-group__input" />
+              </div>
+
+              <div className="form-group">
+                <label className="form-group__label">Fecha Liberación</label>
+                <input type="text" className="form-group__input" />
+              </div>
+            </div>
+            <div className={styles.col}></div>
+          </div>
+          <div className={styles.footer}>
+            <Button type="secondary" onClick={() => {}}>
+              Reiniciar
+            </Button>
+            <Button onClick={() => {}}>Enviar</Button>
+          </div>
+        </form>
+      </>
+    </Page>
+  );
 };
 
 export default ProductDetails;
